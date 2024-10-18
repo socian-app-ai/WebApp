c21e0248459087f95e31541df9d9a77f4fd24482
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-10-19 02:33:58 +0500

    react front end beyond the class

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+<h6>USE</h6>
+
 We will be using pnpm instead of npm. Better choice for monorepo and efficiency
 
 installation url: https://pnpm.io/installation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<h6>No time for this</h6>
+
+We will be using Next.js in addition to React.js with server side rendering capabilites. 
+This will optimise the api calls, database calls. Moreover it has better integration with vercel
+
+With our past experience, we know that mern framework CSR is diificult to integrate with vercel or netlify.
+First reason is cors errors, second is gateway timeout due to late response from server to database and back to server
---
+++ apps/web/beyond-the-class/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: "3000",
+    proxy: {
+      '/api': {
+        target: 'https://url-here-of-backend-in-production',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path
+      },
+    }
+  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+})
